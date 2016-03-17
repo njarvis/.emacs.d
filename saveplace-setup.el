b6b4d445995c5de8bf3cc9f056691fcf196c86d5
Neil Jarvis <neil@senient.com>
2016-03-17 13:52:24 +0000

    Properly enable savehist mode

--- saveplace-setup.el
+++ saveplace-setup.el
@@ -5,14 +5,13 @@
  save-place-limit 10)
 (setq-default save-place t)            ;; activate it for all buffers
 
-(if (>= emacs-major-version '22)
-    ;;
-    ;; savehist: save some history
-    (require 'savehist)
-    (setq savehist-additional-variables    ;; also save...
-	  '(search ring regexp-search-ring);; ... my search entries
-	  savehist-autosave-interval 60    ;; save every minute (default: 5 min)
-	  savehist-file (format "~/.emacs.d/cache/savehist.%s" (system-name))) ;; keep my home clean
-    (savehist-mode t)                ;; do customization before activation
-    )
+;;
+;; savehist: save some history
+(require 'savehist)
+(setq savehist-additional-variables    ;; also save...
+      '(search ring regexp-search-ring);; ... my search entries
+      savehist-autosave-interval 60    ;; save every minute (default: 5 min)
+      savehist-file (format "~/.emacs.d/cache/savehist.%s" (system-name))) ;; keep my home clean
+(savehist-mode t)                ;; do customization before activation
+
 
